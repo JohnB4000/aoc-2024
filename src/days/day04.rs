70df64d82ecd3247e9d363a0125ee1a96a989259
John B <jb2069@hw.ac.uk>
2024-12-04 22:23:20 +0000

    Improve part 1

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -28,23 +28,14 @@ impl Day for Day04 {
 
         for i in 0..input.len() as i32 {
             for j in 0..input[0].len() as i32 {
-                if input[i as usize][j as usize] == 'X' {
+                if *get_char(input, i, j).unwrap_or(&' ') == 'X' {
                     for x in -1..2 as i32 {
                         for y in -1..2 as i32 {
-                            if let Some(char) = get_char(input, i + x, j + y) {
-                                if char == 'M' {
-                                    if let Some(char) = get_char(input, i + 2 * x, j + 2 * y) {
-                                        if char == 'A' {
-                                            if let Some(char) =
-                                                get_char(input, i + 3 * x, j + 3 * y)
-                                            {
-                                                if char == 'S' {
-                                                    count += 1;
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+                            if *get_char(input, i + x, j + y).unwrap_or(&' ') == 'M'
+                                && *get_char(input, i + 2 * x, j + 2 * y).unwrap_or(&' ') == 'A'
+                                && *get_char(input, i + 3 * x, j + 3 * y).unwrap_or(&' ') == 'S'
+                            {
+                                count += 1;
                             }
                         }
                     }
@@ -74,16 +65,16 @@ impl Day for Day04 {
     }
 }
 
-fn get_char(input: &Vec<Vec<char>>, i: i32, j: i32) -> Option<char> {
+fn get_char(input: &Vec<Vec<char>>, i: i32, j: i32) -> Option<&char> {
     if i < 0 || j < 0 {
         return None;
     }
 
-    input.get(i as usize)?.get(j as usize).copied()
+    input.get(i as usize)?.get(j as usize)
 }
 
 fn has_mas(input: &Vec<Vec<char>>, i: i32, j: i32, xy: &[(i32, i32)]) -> bool {
-    let mut previous: Option<char> = None;
+    let mut previous: Option<&char> = None;
 
     for (x, y) in xy {
         if let Some(current) = get_char(input, i + x, j + y) {
