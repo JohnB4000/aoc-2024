f85ad9691ff79acd94c03a05d8b7091479b070ab
John B <jb2069@hw.ac.uk>
2024-12-22 21:05:13 +0000

    Complete day 10

--- src/days/day10.rs
+++ src/days/day10.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashSet,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -8,34 +9,127 @@ use super::Day;
 pub struct Day10;
 
 impl Day for Day10 {
-    type ParsedData = String;
+    type ParsedData = Vec<Vec<u32>>;
 
     type Part1Result = i32;
 
     type Part2Result = i32;
 
     fn parse(input_directory: String) -> Result<Self::ParsedData, String> {
-        let file = match File::open(input_directory + "day10.txt") {
-            Ok(file) => file,
-            Err(e) => return Err(e.to_string()),
-        };
-        let reader = BufReader::new(file);
-
-        for line in reader.lines() {
-            match line {
-                Ok(line) => todo!(),
-                Err(e) => todo!(),
-            }
-        }
-
-        Ok(String::new())
+        Ok(
+            BufReader::new(File::open(input_directory + "day10.txt").expect("File Error"))
+                .lines()
+                .map(|line| {
+                    line.unwrap()
+                        .chars()
+                        .map(|value| value.to_digit(10).unwrap())
+                        .collect()
+                })
+                .collect(),
+        )
     }
 
     fn part_1(input: &Self::ParsedData) -> Self::Part1Result {
-        todo!()
+        let mut result = 0;
+
+        for (row, arr) in input.iter().enumerate() {
+            for (col, value) in arr.iter().enumerate() {
+                if *value != 0 {
+                    continue;
+                }
+
+                result += search_part_1(input, (row, col));
+            }
+        }
+
+        result
     }
 
     fn part_2(input: &Self::ParsedData) -> Self::Part2Result {
-        todo!()
+        let mut result = 0;
+
+        for (row, arr) in input.iter().enumerate() {
+            for (col, value) in arr.iter().enumerate() {
+                if *value != 0 {
+                    continue;
+                }
+
+                result += search_part_2(input, (row, col));
+            }
+        }
+
+        result
     }
+}
+
+fn get_neighbours(input: &Vec<Vec<u32>>, row: usize, col: usize) -> Vec<(usize, usize)> {
+    let mut neighbours = Vec::new();
+
+    if row + 1 < input.len() {
+        neighbours.push((row + 1, col));
+    }
+    if col + 1 < input[0].len() {
+        neighbours.push((row, col + 1));
+    }
+
+    if row > 0 {
+        neighbours.push((row - 1, col));
+    }
+    if col > 0 {
+        neighbours.push((row, col - 1));
+    }
+
+    neighbours
+}
+
+fn search_part_1(input: &Vec<Vec<u32>>, coords: (usize, usize)) -> i32 {
+    let (row, col) = coords;
+    let mut count = 0;
+
+    let mut queue = Vec::new();
+    queue.push((row, col));
+
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+
+    while let Some((row, col)) = queue.pop() {
+        if visited.contains(&(row, col)) {
+            continue;
+        }
+
+        visited.insert((row, col));
+
+        if input[row][col] == 9 {
+            count += 1;
+        }
+
+        for (neighbour_row, neighbour_col) in get_neighbours(input, row, col) {
+            if input[row][col] + 1 == input[neighbour_row][neighbour_col] {
+                queue.push((neighbour_row, neighbour_col));
+            }
+        }
+    }
+
+    count
+}
+
+fn search_part_2(input: &Vec<Vec<u32>>, coords: (usize, usize)) -> i32 {
+    let (row, col) = coords;
+    let mut count = 0;
+
+    let mut queue = Vec::new();
+    queue.push((row, col));
+
+    while let Some((row, col)) = queue.pop() {
+        if input[row][col] == 9 {
+            count += 1;
+        }
+
+        for (neighbour_row, neighbour_col) in get_neighbours(input, row, col) {
+            if input[row][col] + 1 == input[neighbour_row][neighbour_col] {
+                queue.push((neighbour_row, neighbour_col));
+            }
+        }
+    }
+
+    count
 }
--- src/days/day11.rs
+++ src/days/day11.rs
@@ -8,34 +8,74 @@ use super::Day;
 pub struct Day11;
 
 impl Day for Day11 {
-    type ParsedData = String;
+    type ParsedData = Vec<u64>;
 
-    type Part1Result = i32;
+    type Part1Result = usize;
 
-    type Part2Result = i32;
+    type Part2Result = usize;
 
     fn parse(input_directory: String) -> Result<Self::ParsedData, String> {
-        let file = match File::open(input_directory + "day11.txt") {
-            Ok(file) => file,
-            Err(e) => return Err(e.to_string()),
-        };
-        let reader = BufReader::new(file);
-
-        for line in reader.lines() {
-            match line {
-                Ok(line) => todo!(),
-                Err(e) => todo!(),
-            }
-        }
-
-        Ok(String::new())
+        Ok(
+            BufReader::new(File::open(input_directory + "day11.txt").expect("File Error"))
+                .lines()
+                .flat_map(|line| {
+                    line.unwrap()
+                        .split_whitespace()
+                        .map(|stone| stone.parse().unwrap())
+                        .collect::<Vec<u64>>()
+                })
+                .collect(),
+        )
     }
 
     fn part_1(input: &Self::ParsedData) -> Self::Part1Result {
-        todo!()
+        let process = |input: Vec<u64>| -> Vec<u64> {
+            input
+                .iter()
+                .flat_map(|stone| match stone {
+                    0 => vec![1],
+                    _ if stone.to_string().len() % 2 == 0 => {
+                        let mut temp = stone.to_string().clone();
+                        let other = temp.split_off(temp.len() / 2);
+                        vec![temp.parse().unwrap(), other.parse().unwrap()]
+                    }
+                    _ => vec![stone * 2024],
+                })
+                .collect::<Vec<u64>>()
+        };
+
+        let mut input = input.clone();
+
+        for _ in 0..25 {
+            input = process(input);
+        }
+
+        input.len()
     }
 
     fn part_2(input: &Self::ParsedData) -> Self::Part2Result {
-        todo!()
+        // let process = |input: Vec<u64>| -> Vec<u64> {
+        //     input
+        //         .par_iter()
+        //         .flat_map(|stone| match stone {
+        //             0 => vec![1],
+        //             _ if stone.to_string().len() % 2 == 0 => {
+        //                 let mut temp = stone.to_string().clone();
+        //                 let other = temp.split_off(temp.len() / 2);
+        //                 vec![temp.parse().unwrap(), other.parse().unwrap()]
+        //             }
+        //             _ => vec![stone * 2024],
+        //         })
+        //         .collect::<Vec<u64>>()
+        // };
+        //
+        // let mut input = input.clone();
+        //
+        // for i in 0..75 {
+        //     input = process(input);
+        //     dbg!(i);
+        // }
+
+        input.len()
     }
 }
