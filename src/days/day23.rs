8515ab3d629406ec86d73c2de8afd747deff2da4
John B <jb2069@hw.ac.uk>
2024-12-24 14:58:37 +0000

    Complete day 24 part 1

--- src/days/day23.rs
+++ src/days/day23.rs
@@ -1,41 +1,108 @@
 use std::{
+    collections::{HashMap, HashSet},
     fs::File,
     io::{BufRead, BufReader},
 };
+
+use regex::Regex;
 
 use super::Day;
 
 pub struct Day23;
 
 impl Day for Day23 {
-    type ParsedData = String;
+    type ParsedData = HashMap<String, Vec<String>>;
 
     type Part1Result = i32;
 
-    type Part2Result = i32;
+    type Part2Result = String;
 
     fn parse(input_directory: String) -> Result<Self::ParsedData, String> {
-        let file = match File::open(input_directory + "day23.txt") {
-            Ok(file) => file,
-            Err(e) => return Err(e.to_string()),
-        };
-        let reader = BufReader::new(file);
+        let mut connections = HashMap::new();
+        let re = Regex::new(r"(\w+)-(\w+)").unwrap();
+        BufReader::new(File::open(input_directory + "day23test.txt").expect("File Error"))
+            .lines()
+            .for_each(|line| {
+                re.captures_iter(&line.unwrap()).for_each(|c| {
+                    let comp1 = c.get(1).unwrap().as_str().to_string();
+                    let comp2 = c.get(2).unwrap().as_str().to_string();
+                    connections
+                        .entry(comp1.clone())
+                        .or_insert_with(Vec::new)
+                        .push(comp2.clone());
+                    connections
+                        .entry(comp2)
+                        .or_insert_with(Vec::new)
+                        .push(comp1);
+                })
+            });
 
-        for line in reader.lines() {
-            match line {
-                Ok(line) => todo!(),
-                Err(e) => todo!(),
-            }
-        }
-
-        Ok(String::new())
+        Ok(connections)
     }
 
     fn part_1(input: &Self::ParsedData) -> Self::Part1Result {
-        todo!()
+        let computers: Vec<&String> = input.keys().collect();
+        let mut groups = HashSet::new();
+
+        for computer in computers {
+            let neighbours = input.get(computer).unwrap();
+
+            for neighbour1 in neighbours {
+                for neighbour2 in neighbours {
+                    if neighbour1 == neighbour2 {
+                        continue;
+                    }
+
+                    if input.get(neighbour1).unwrap().contains(neighbour2) {
+                        let mut group =
+                            vec![computer.as_str(), neighbour1.as_str(), neighbour2.as_str()];
+                        group.sort();
+                        groups.insert(group);
+                    }
+                }
+            }
+        }
+
+        groups
+            .iter()
+            .filter(|computers| {
+                computers
+                    .iter()
+                    .filter(|computer| computer.chars().next().unwrap() == 't')
+                    .collect::<Vec<&&str>>()
+                    .len()
+                    > 0
+            })
+            .count() as i32
     }
 
     fn part_2(input: &Self::ParsedData) -> Self::Part2Result {
-        todo!()
+        let computers: Vec<&String> = input.keys().collect();
+
+        let mut longest = 0;
+        let mut current = Vec::new();
+
+        for computer in computers {
+            if let (_, Some(path)) = dfs(input, computer, HashSet::new()) {
+                if path.len() > longest {
+                    longest = path.len();
+                    current = path;
+                }
+            }
+        }
+
+        String::new()
     }
+}
+
+fn dfs(
+    input: &HashMap<String, Vec<String>>,
+    computer: &str,
+    visited: HashSet<&str>,
+) -> (bool, Option<Vec<String>>) {
+    if visited.contains(computer) {
+        return (false, None);
+    }
+
+    (false, None)
 }
--- src/days/day24.rs
+++ src/days/day24.rs
@@ -1,38 +1,115 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
+use regex::Regex;
+
 use super::Day;
+
+#[derive(Debug, Clone)]
+pub enum Operation {
+    AND,
+    OR,
+    XOR,
+}
 
 pub struct Day24;
 
 impl Day for Day24 {
-    type ParsedData = String;
+    type ParsedData = (
+        HashMap<String, u64>,
+        Vec<(String, Operation, String, String)>,
+    );
 
-    type Part1Result = i32;
+    type Part1Result = u64;
 
-    type Part2Result = i32;
+    type Part2Result = u64;
 
     fn parse(input_directory: String) -> Result<Self::ParsedData, String> {
-        let file = match File::open(input_directory + "day24.txt") {
-            Ok(file) => file,
-            Err(e) => return Err(e.to_string()),
-        };
-        let reader = BufReader::new(file);
+        let mut state = HashMap::new();
+        let mut instructions = Vec::new();
 
-        for line in reader.lines() {
-            match line {
-                Ok(line) => todo!(),
-                Err(e) => todo!(),
-            }
-        }
+        let re = Regex::new(r"(?:(\w+): (\d))|(?:(\w+) (XOR|OR|AND) (\w+) -> (\w+))").unwrap();
+        BufReader::new(File::open(input_directory + "day24.txt").expect("File Error"))
+            .lines()
+            .for_each(|line| {
+                re.captures_iter(&line.unwrap()).for_each(|c| {
+                    if let Some(reg) = c.get(1) {
+                        state.insert(
+                            reg.as_str().to_string(),
+                            c.get(2).unwrap().as_str().parse().unwrap(),
+                        );
+                    } else {
+                        instructions.push((
+                            c.get(3).unwrap().as_str().to_string(),
+                            match c.get(4).unwrap().as_str() {
+                                "AND" => Operation::AND,
+                                "OR" => Operation::OR,
+                                "XOR" => Operation::XOR,
+                                _ => todo!(),
+                            },
+                            c.get(5).unwrap().as_str().to_string(),
+                            c.get(6).unwrap().as_str().to_string(),
+                        ));
+                    }
+                })
+            });
 
-        Ok(String::new())
+        Ok((state, instructions))
     }
 
     fn part_1(input: &Self::ParsedData) -> Self::Part1Result {
-        todo!()
+        let (mut state, mut instructions) = input.clone();
+
+        while instructions.len() > 0 {
+            instructions = instructions
+                .iter()
+                .filter_map(|(left, op, right, result)| {
+                    if !state.contains_key(left) || !state.contains_key(right) {
+                        Some((
+                            left.to_string(),
+                            op.clone(),
+                            right.to_string(),
+                            result.to_string(),
+                        ))
+                    } else {
+                        state.insert(
+                            result.to_string(),
+                            match op {
+                                Operation::AND => {
+                                    state.get(left).unwrap() & state.get(right).unwrap()
+                                }
+                                Operation::OR => {
+                                    state.get(left).unwrap() | state.get(right).unwrap()
+                                }
+                                Operation::XOR => {
+                                    state.get(left).unwrap() ^ state.get(right).unwrap()
+                                }
+                            },
+                        );
+                        None
+                    }
+                })
+                .collect();
+        }
+
+        let result = state
+            .iter()
+            .filter(|(key, value)| key.chars().next().unwrap() == 'z')
+            .collect::<Vec<(&String, &u64)>>();
+
+        let mut number = vec![0; result.len()];
+
+        result.iter().for_each(|(key, value)| {
+            let (_, index) = key.split_at(1);
+            number[index.parse::<usize>().unwrap()] = **value;
+        });
+
+        number.reverse();
+        let number: String = number.iter().map(|num| num.to_string()).collect();
+        u64::from_str_radix(&number, 2).unwrap()
     }
 
     fn part_2(input: &Self::ParsedData) -> Self::Part2Result {
